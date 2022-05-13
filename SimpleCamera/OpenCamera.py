99b07ae854372cce2ac62669e054a0b842d6d7be
Mahimai Raja J <mahimairaja3@gmail.com>
2022-05-13 12:40:53 +0530

    Live Stream with USB-CAM

---
+++ SimpleCamera/OpenCamera.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+def show_cam():
+    id = '/dev/video0'  # location of the USB cam connected
+    video = cv.VideoCapture(id, cv.CAP_V4L2)  # V4L2 - Video for Linux ver-2
+    while True :
+        return_val, frame = video.read() # read function returns to out , only fram is useful here
+        cv.imshow("iKurious-EYE", frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+    video.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    show_cam()
